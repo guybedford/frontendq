e203a7e7a26a18b542b3dac90fa618e16bf0094e
Lars Francke <lars.francke@gmail.com>
2015-02-14 14:00:25 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@1.3.13"
+  }
+});
+
